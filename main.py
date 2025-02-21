b2582788b29b0980a0f9fd293d828c195090bb8b
TiffanyClark225 <91997752+TiffanyClark225@users.noreply.github.com>
2025-02-21 18:30:39 +0000

    Added flight data

---
+++ main.py
@@ -0,0 +1,23 @@
+# Take the input from the flights.txt file and parse it
+# The input is in the format of: <day of week, month, day of month, departure time; carrier; price; flight arrival time; day of week, month, day of month, departure time; departure arrival time>
+# The output should be the best flight based on total price (ticket, parking, rental car)
+
+# First, we need to parse the input file
+
+
+
+
+
+# Second, we need to calculate the total price of each flight
+# Take the flight ticket price from the file then use if/else statements to add parking and transportation
+# Parking is $12 per day
+# If we arrive before 9AM, I need to add $30 for an uber
+# If we arrive after 8PM, I need to add $30 for a rental car
+# Else, $0 for transportation
+
+
+
+
+# Third, we need to find the best flight based on total price
+# The best flight is the one with the lowest total price
+
